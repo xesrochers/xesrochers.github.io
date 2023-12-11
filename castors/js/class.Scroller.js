0ebafb6ac9f3ac4f9041df57f88e19f654c49276
Sly <xesrochers@gmail.com>
2023-12-10 19:16:25 -0500

    Castors - New Scroll code

--- castors/js/class.Scroller.js
+++ castors/js/class.Scroller.js
@@ -9,6 +9,7 @@ function Scroller() {
  ************************************************/
 Scroller.rate = 5000; 
 Scroller.snooze = 5000;
+Scroller.wait  = 5000;
 Scroller.timeout = null;
 
 
@@ -32,6 +33,15 @@ Scroller.setState = function(state) {
 
 
 /************************************************
+ * fillBar()
+ ************************************************/
+Scroller.fillBar = function() {
+	var percent = (Scroller.wait/Scroller.snooze)*100;
+	$('.bar').css('width', ''+percent+'%');
+}
+
+
+/************************************************
  * setControls()
  ************************************************/
 Scroller.setControls = function(state) {
@@ -40,35 +50,54 @@ Scroller.setControls = function(state) {
 	if (state == 'reset') {
 		playBtn.removeClass("blink");
 		playBtn.attr('title', 'play');
-		playBtn.find('i').attr("class", "").addClass('icon-play');
-		$('.tab-slide').removeClass("hidden");
+		playBtn.find('.icon').removeClass('bar').html('start');
+		$('.tab-handle').show(300);
 	} else if (state == 'snoozing') {
 		playBtn.addClass("blink");
 		playBtn.attr('title', 'pause');
-		playBtn.find('i').attr("class", "").addClass('icon-timer');
-		$('.tab-slide').addClass("hidden");
+		playBtn.find('.icon').addClass('bar');
+		Scroller.fillBar();
+		$('.tab-handle').hide(300);
 	} else if (state == 'paused') {
 		playBtn.removeClass("blink");
-		playBtn.attr('title', 'play');
-		playBtn.find('i').attr("class", "").addClass('icon-play');
-		$('.tab-slide').removeClass("hidden");
+		playBtn.attr('title', 'resume');
+		playBtn.find('.icon').removeClass('bar').html('resume');
+		$('.tab-handle').show(300);
 	} else if (state == 'playing') {
 		playBtn.addClass('blink');
 		playBtn.attr('title', 'pause');
-		playBtn.find('i').attr("class", "").addClass('icon-pause');
-		$('.tab-slide').addClass("hidden");
+		playBtn.find('.icon').removeClass('bar').html('pause');
+		$('.tab-handle').hide(300);
 		$('.tablature').addClass('hidden');
 		$('#harmony-control').addClass('hidden');
 	} else if (state == 'completed') {
 		playBtn.removeClass("blink");
-		playBtn.attr('title', 'reset');
-		playBtn.find('i').attr("class", "").addClass('icon-arrow-up');
-		$('.tab-slide').removeClass("hidden");
+		playBtn.attr('title', 'top');
+		playBtn.find('.icon').removeClass('bar').html('top');
+		$('.tab-handle').show(300);
 		$('.tablature').addClass('hidden');
 		$('#harmony-control').addClass('hidden');
 	}
 
 }
+
+/************************************************
+ * snoozy()
+ ************************************************/
+Scroller.snoozy = function() {
+
+	var rate = 2000;
+	if (Scroller.wait > 0) {
+		Scroller.setState('snoozing');
+		Scroller.setControls('snoozing');
+		Scroller.wait -= 1000;
+		Scroller.timeout = setTimeout(Scroller.snoozy, 2000);
+	} else {
+		Scroller.start();
+	}
+}
+
+
 
 /************************************************
  * play()
@@ -81,9 +110,9 @@ Scroller.play = function(e) {
 	console.log('current state is ' + state);
 
 	if (state == 'reset') {
-		Scroller.timeout = setTimeout(Scroller.start, Scroller.snooze);
-		Scroller.setState('snoozing');
-		Scroller.setControls('snoozing');
+		Scroller.snoozy();
+	} else if (state == 'snoozing') {
+		Scroller.snoozy();
 	} else if (state == 'playing') {
 		Scroller.pause();
 	} else if (state == 'paused') {
@@ -179,6 +208,7 @@ Scroller.updateSnooze = function(e) {
  ************************************************/
 Scroller.setSnooze = function(val) {
 	Scroller.snooze = val;
+	Scroller.wait = val;
 	console.log('snooze changed to ' + val);
 	$("#snooze").html(Math.floor(val/1000) + " sec");
 	$('.snooze').val(val);
@@ -243,34 +273,22 @@ Scroller.readStorage = function(key, def) {
 	return result;
 }
 
+/************************************************
+ * saveCookie()
+ ************************************************/
+Scroller.saveCookie = function(key, def) {
+	var domainName = window.location.hostname;
+	document.cookie = key + '=' + def +'; path=/; domain=.' + domainName;
+}
 
 /************************************************
- * init()
+ * readCookie()
  ************************************************/
-Scroller.init = function() {
-	var rate = Scroller.rate;
-	var snooze = Scroller.snooze;
-
-	if (typeof(Storage) !== "undefined") {
-		rate = Scroller.readStorage("rate", Scroller.rate);
-		snooze = Scroller.readStorage("snooze", Scroller.snooze);
-	} else {
-		$(".rate").hide(500);
-	}
-
-   var config = $('#config');
-	if (config.length) {
-	   	rate  = Scroller.readServer("rate", Scroller.rate);
-	   	snooze = Scroller.readServer('snooze', Scroller.snooze);
-	   	console.log('rate and snooze restored from configuration');
-	   	Scroller.disableControls(false);
-	   	$('#js-server-option').show();
-	} else {
-	   	$('#js-server-option').hide();
-	}
-
-	Scroller.setRate(rate);
-	Scroller.setSnooze(snooze);
+Scroller.readCookie = function(key, def) {
+ // Get name followed by anything except a semicolon
+  var cookiestring=RegExp(key+"=[^;]+").exec(document.cookie);
+  // Return everything after the equal sign, or an empty string if the cookie name not found
+  return decodeURIComponent(!!cookiestring ? cookiestring.toString().replace(/^[^=]+./,"") : "");
 }
 
 /************************************************
@@ -306,6 +324,60 @@ Scroller.flipConfig = function(e) {
 }
 
 /************************************************
+ * autoPlay()
+ ************************************************/
+Scroller.autoPlay = function() {
+	var result = false;
+	var cookie = Scroller.readCookie('autoPlay');
+	if (cookie != null) {
+		result = cookie == 'true';
+	}
+
+	return result;
+}
+
+
+/************************************************
+ * setAutoPlay()
+ ************************************************/
+Scroller.setAutoPlay = function(e) {
+	if (this.checked) {
+		Scroller.saveCookie('autoPlay', 'true');
+	} else {
+		Scroller.saveCookie('autoPlay', 'false');		
+	}
+}
+
+/************************************************
+ * init()
+ ************************************************/
+Scroller.init = function() {
+	var rate = Scroller.rate;
+	var snooze = Scroller.snooze;
+
+	if (typeof(Storage) !== "undefined") {
+		rate = Scroller.readStorage("rate", Scroller.rate);
+		snooze = Scroller.readStorage("snooze", Scroller.snooze);
+	} else {
+		$(".rate").hide(500);
+	}
+
+   var config = $('#config');
+	if (config.length) {
+	   	rate  = Scroller.readServer("rate", Scroller.rate);
+	   	snooze = Scroller.readServer('snooze', Scroller.snooze);
+	   	console.log('rate and snooze restored from configuration');
+	   	Scroller.disableControls(false);
+	   	$('#js-server-option').show();
+	} else {
+	   	$('#js-server-option').hide();
+	}
+
+	Scroller.setRate(rate);
+	Scroller.setSnooze(snooze);
+}
+
+/************************************************
  * wireup()
  ************************************************/
 Scroller.wireup = function() {
@@ -316,15 +388,19 @@ Scroller.wireup = function() {
 	$('.rate').change(Scroller.updateRate);  
 	$('.snooze').change(Scroller.updateSnooze);
 
+	$('#js-auto-play').click(Scroller.setAutoPlay);
 	$('#js-server').click(Scroller.flipConfig);
 	$('#refresh').click(Scroller.refresh);
 
 	if ($(".chord").length == 0) { // check if on song page
 		$(".controls").hide();
-		$(".tab-slide").hide();
+		$(".tab-handle").hide();
+	} else {
+		$("#searchbox-js").hide();
 	}
 
-	if (Url.qs('play') != "") {
+	if (Scroller.autoPlay()) {
+		$("#js-auto-play").attr('checked', 'checked');
 		$('.play').click();
 	}
 }
--- castors/js/class.TabSlide.js
+++ castors/js/class.TabSlide.js
@@ -22,7 +22,7 @@ TabSlide.init = function() {
             tabLocation: 'right',                       //side of screen where tab lives, top, right, bottom, or left
             speed: 300,                                 //speed of animation
             action: 'click',                            //options: 'click' or 'hover', action to trigger animation
-            topPos: '10%',                              //position from the top/ use if tabLocation is left or right
+            topPos: '40px',                              //position from the top/ use if tabLocation is left or right
             leftPos: '20px',                            //position from left/ use if tabLocation is bottom or top
             fixedPosition: true                         //options: true makes it stick(fixed position) on scroll
         });
@@ -30,10 +30,25 @@ TabSlide.init = function() {
 
 
 /************************************************
+ * toggle()
+ ************************************************/
+TabSlide.toggle = function(e) {
+   $('.tab-slide').toggle(300);
+   var hook = $('.tab-handle').html();
+   if (hook == "&lt;") {
+      $('.tab-handle').html("X");
+   } else {
+      $('.tab-handle').html("&lt;");
+   }
+}
+
+
+/************************************************
  * wireup()
  ************************************************/
 TabSlide.wireup = function() {
-	TabSlide.init();
+   $('.tab-slide').hide();
+   $('.tab-handle').click(TabSlide.toggle);
 }
 
 /************************************************
