75ba0c0342dc8b901459902c91448fac9104765d
Sly <xesrochers@gmail.com>
2024-04-07 21:54:09 -0400

    Yuliya - Lightbox

--- yuliya/js/class.Gallery.js
+++ yuliya/js/class.Gallery.js
@@ -4,7 +4,6 @@
 function Gallery() {
 }
 
-
 /************************************************
  * applyFilter()
  ************************************************/
@@ -22,34 +21,14 @@ Gallery.applyFilter = function(e) {
       $('#gallery .item').hide();
       $('#gallery .item.'+filter).show(); 
    }
-
-}
-
-/************************************************
- * show()
- ************************************************/
-Gallery.show = function(e) {
-   e.preventDefault();
-   var model = $(this).attr('href');
-   $(model).show(500);
-   //$('#gallery .modal.'+id).show(500);
-}
-
-/************************************************
- * close()
- ************************************************/
-Gallery.close = function(e) {
-   e.preventDefault();
-   $('#gallery .modal').hide(500);
 }
 
 /************************************************
  * wireup()
  ************************************************/
 Gallery.wireup = function() {
-   $('#gallery .view').click(Gallery.show);
-   $('#gallery .close').click(Gallery.close);
-   $('#gallery #filters button').click(Gallery.applyFilter);
+   $('#gallery #filters .button').click(Gallery.applyFilter);
+   lightbox.option({'resizeDuration': 200,'wrapAround': true});
 }
 
 /************************************************
