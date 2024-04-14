9319b833cabbe0abf0a1d9d59df331982eb9c7e1
Sly <xesrochers@gmail.com>
2024-04-14 17:52:09 -0400

    Yuliya - Ready for presentation

--- yuliya/js/class.Random.js
+++ yuliya/js/class.Random.js
@@ -4,18 +4,18 @@
 function Random() {
 }
 
-Random.Colors = [
-    "#ffd7d5",
-    "#ffe9d6",
-    "#ffffd1",
-    "#d6ffda",
-    "#d7eeff",
-    "#dad6ff",
-    "#ffd6e8",
-    "#f5f5dc",
-    "#f4e4e4",
-    "#e4e6f4" 
-  ];
+Random.Colors = [ "#b18e50", "#1eaedb" ];
+  //   "#ffd7d5",
+  //   "#ffe9d6",
+  //   "#ffffd1",
+  //   "#d6ffda",
+  //   "#d7eeff",
+  //   "#dad6ff",
+  //   "#ffd6e8",
+  //   "#f5f5dc",
+  //   "#f4e4e4",
+  //   "#e4e6f4" 
+  // ];
 
 
 /************************************************
