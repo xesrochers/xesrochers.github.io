82793a0b11a7d41399edc1caa68f44f3a6a5e847
Sly <xesrochers@gmail.com>
2024-04-17 10:30:07 -0400

    Yuliya - Bring back social icons

---
+++ yuliya/js/class.Contact.js
@@ -0,0 +1,28 @@
+/************************************************
+ * Contact class definition
+ ************************************************/
+function Contact() {
+}
+
+Contact.email = 'mailto::info@yuliyafineart.com';
+
+/************************************************
+ * contact()
+ ************************************************/
+Contact.contact = function(e) {
+   window.location.replace(Contact.email);
+}
+
+/************************************************
+ * wireup()
+ ************************************************/
+Contact.wireup = function() {
+   $('#contact a').click(Contact.contact);
+}
+
+/************************************************
+ * jQuery.ready()
+ ************************************************/
+$(function() {
+   Contact.wireup();
+});
